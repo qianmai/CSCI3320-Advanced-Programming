54bbdcf4dfa322164b9e39f30e15329cdb251018
qianmai <chennymai@gmail.com>
2015-03-08 18:26:48 -0600

    Game Activity

--- TicTacToe/app/src/main/java/TicTacToeActivity.java
+++ TicTacToe/app/src/main/java/TicTacToeActivity.java
@@ -1,16 +1,19 @@
-package com.example.qianmai.tictactoe;
 
+/**
+ * Created by Qian Mai on 3/8/2015.
+ */
+//package va.indiedevelopment.tictactoe;
+
+import android.app.Activity;
 import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class TicTacToeTutorialActivity extends Activity {
+
     private TicTacToeGame mGame;
 
     private Button mBoardButtons[];
@@ -149,32 +152,4 @@ public class MainActivity extends ActionBarActivity {
         else
             mBoardButtons[location].setTextColor(Color.RED);
     }
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
